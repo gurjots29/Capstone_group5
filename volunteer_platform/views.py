36bf998b5f3f99422745f567f1a45aec8cf56cf1
Claudia Villegas <c.villegasrascon@student.fdu.edu>
2023-10-08 20:40:58 -0700

    Frontend: Login and user menu in the home page

--- volunteer_platform/views.py
+++ volunteer_platform/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
+
 
 def home(request):
     return render(request, 'home.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('home')
+                else:
+                    return render(request, 'login.html', {'form': form, 'error': 'Cuenta desactivada'})
+            else:
+                return render(request, 'login.html', {'form': form, 'error': 'Credenciales inválidas'})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
