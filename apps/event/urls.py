93b0e628054c4266da4b1fcfb7e805e2fe74e5cc
Claudia Villegas <c.villegasrascon@student.fdu.edu>
2023-11-22 16:48:03 -0800

    Fix API event and add user security

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path 
 
-from .views import EventViewSet, RegistrationEventsViewSet, ProgramViewSet,events_management_view
+from .views import (EventViewSet, RegistrationEventsViewSet, ProgramViewSet,
+                   events_management_view,CreateEventView,events_view)
 
 urlpatterns = [
    path('events/', EventViewSet.as_view(), name='api_createevent'),
+  
+
    path('registration-events/', RegistrationEventsViewSet.as_view(), name='api_registrationevent'),
    path('programs/', ProgramViewSet.as_view(), name='api_programs'),
 
-    path('events-management/', events_management_view, name='events-management'),
+
+   path('new/', CreateEventView.as_view(), name='create-event'),
+   path('events-management/', events_management_view, name='events-management'),
+   path('view-events', events_view, name='view-events'),
+
 
 ]
